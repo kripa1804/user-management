cab5ed5639ff4f33658e6db2bf75357b2fe5230c
kripa18 <mailkripa.18@gmail.com>
2024-10-14 08:20:45 -0400

    build fix

--- src/test/java/com/example/user_management/UserManagementApplicationTests.java
+++ src/test/java/com/example/user_management/UserManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.user_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UserManagementApplication.class)
 class UserManagementApplicationTests {
 
 	@Test
